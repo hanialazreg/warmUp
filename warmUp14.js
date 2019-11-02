a8553197802f2a371fcb8ba45af2e9ca2ba83086
hanialazreg <lazreghania@gmail.com>
2019-11-02 09:55:01 +0100

    warmUp14

--- warmUp14.js
+++ warmUp14.js
@@ -19,5 +19,34 @@ Explanation: In this case, no transaction is done, i.e. max profit = 0.
 */
 
 var maxProfit = function(prices) {
-  // YOUR CODE HERE
+	var max = 0;
+	var min =0 ;
+	var day_max = 0;
+	var day_min = 0 ;
+	var maxPro = 0;
+	
+	// find day of minimum price => day of buy
+	// we can't sell befor buy
+	for (var i = 0; i < prices.length; i++) {
+		if(prices[i] < min ){
+			min = prices[i];
+			day_min = i ;
+		}
+	}
+
+	// if it is not the last day so we cann do the operation 
+	if(day_min < prices.length -1){
+
+	for (var j = day_min; j < prices.length; j++) {
+		if( prices[j] > max){
+			max = prices[j];
+			day_max = j ;
+		}
+	}
+}
+
+	return maxPro = prices[day_max] - prices[day_min];
+	
 };
+
+
